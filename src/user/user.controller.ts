bd153b4e36adf810b60233076f6f28d05eb365ef
Vitor Augusto <playonadev@gmail.com>
2024-09-18 17:30:30 -0300

    feat: Implement Prisma library for table creation, relationships, and basic CRUD operations with password encryption.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client'
+
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){
+
+    }
+
+    @Post('')
+    async signupUser(
+        @Body() userData: Prisma.UserCreateInput,
+    ): Promise<UserModel>{
+        return this.userService.createUser(userData);
+    }
+
+
+    @Get(':id')
+    async getUser(@Param('id') id: string): Promise<UserModel> {
+        return this.userService.user({ id: Number(id)});
+    }
+    
+    @Put()
+    async updateUser(
+        @Body() userData: Prisma.UserUpdateInput,
+        @Param('id') id: string,
+    ): Promise<UserModel> {
+        return this.userService.updateUser({ 
+            where: {id: Number(id)}, 
+            data: userData});
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string): Promise<UserModel> {
+        return this.userService.deleteUser({ id: Number(id)});
+    }
+}
